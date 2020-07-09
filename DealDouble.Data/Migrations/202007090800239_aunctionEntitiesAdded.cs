d2d67ad3c42f2dc983934f782c867ed30ee8de5d
Umar Farooq <farooqu475@gmail.com>
2020-07-09 14:47:02 +0500

    Added Entities and Data project, install Entity FrameWork in Data and Enabled Migrations

---
+++ DealDouble.Data/Migrations/202007090800239_aunctionEntitiesAdded.cs
@@ -0,0 +1,31 @@
+﻿namespace DealDouble.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class aunctionEntitiesAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Auctions",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        PictureURL = c.String(),
+                        Description = c.String(),
+                        ActualAmount = c.String(),
+                        StartingTime = c.DateTime(nullable: false),
+                        EndTime = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Auctions");
+        }
+    }
+}
